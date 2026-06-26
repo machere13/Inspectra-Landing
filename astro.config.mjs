7ab3172d1a67d0db58b4efe368cafe8a230b37e7
machere13 <vladislav.chubarov13@yandex.ru>
2026-06-26 10:01:21 +0300

    feat: deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
+  site: "https://machere13.github.io",
+  base: "/Inspectra-Landing/",
   scopedStyleStrategy: "class",
 });
